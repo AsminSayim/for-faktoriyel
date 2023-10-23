373d29e4d3e368f4a7f87fdba84a2926b89d6542
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-10-23 15:38:39 +0300

    Add files via upload

---
+++ for-faktiroyel/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_faktiroyel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            int sonuc = 1;
+            sayi = Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i <= sayi; i++)
+            {
+                sonuc *= i;
+            }
+            label1.Text = sonuc.ToString();
+            
+            
+               
+
+        }
+    }
+}
